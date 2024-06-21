8abc41f1dd2a70ef9f4a7f4a639e6c76676f34a6
WilDev <wilfridorafaellawson@gmail.com>
2024-06-21 17:37:26 -0500

    estructura basica

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h3>Hola mundo</h3>
+  )
+}
+
+export default Home
---
+++ src/pages/PaginaBase/index.jsx
@@ -0,0 +1,11 @@
+import AppRouter from "../../router.jsx"
+
+const Base = () => {
+  return (
+    <>
+    <AppRouter/>
+    </>
+  )
+}
+
+export default Base
